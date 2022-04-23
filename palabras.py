387973284fe11b909f054f0d80badf77d8b1be64
Mario <marioccrr4@gmail.com>
2022-04-23 21:48:41 +0200

    contador palabras

---
+++ palabras.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext
+import sys
+
+def main(filename, new_filename):
+    with SparkContext() as sc:
+        sc.setLogLevel("ERROR")
+        data = sc.textFile(filename)
+        words_rdd = data.map(lambda x: len(x.split()))
+        f = open(new_filename,"w")
+        f.write(str(words_rdd.sum()))
+        f.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python3 {0} <file>".format(sys.argv[0]))
+    else:
+        main(sys.argv[1], "out_"+sys.argv[1])
